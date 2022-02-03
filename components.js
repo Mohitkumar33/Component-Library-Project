4588e26e95eff68b9ce04e42e738371660f0e5bc
Mohit kumar <46507817+Mohitkumar33@users.noreply.github.com>
2022-02-03 08:45:06 +0530

    fix: fixed bugs in responsive navbar

--- components.js
+++ components.js
@@ -1,5 +1,5 @@
 const toggleButton = document.getElementsByClassName('toggle-button')[0]       // as toggle button return the array so we want the first element of the array
-const navbarLinks = document.getElementsByClassName('nav-links')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 
 toggleButton.addEventListener('click',()=> {
     navbarLinks.classList.toggle('active')
