5d6845934c80e36511efc85e563e94579fc5aa4d
Mohit kumar <46507817+Mohitkumar33@users.noreply.github.com>
2022-02-07 20:18:08 +0530

    refactor: code beautify

--- components.js
+++ components.js
@@ -1,6 +1,6 @@
-const toggleButton = document.getElementsByClassName('toggle-button')[0]       // as toggle button return the array so we want the first element of the array
+const toggleButton = document.getElementsByClassName('toggle-button')[0] // as toggle button return the array so we want the first element of the array
 const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 
-toggleButton.addEventListener('click',()=> {
+toggleButton.addEventListener('click', () => {
     navbarLinks.classList.toggle('active')
 })
